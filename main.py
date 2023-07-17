cc6052bde94b431ca6c6f449f6a18d4aff08e9bd
supersiem <100954549+supersiem@users.noreply.github.com>
2023-07-17 17:19:11 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -4,38 +4,17 @@ class SpriteKind:
     player1 = SpriteKind.create()
     cam = SpriteKind.create()
     player3 = SpriteKind.create()
-
-def on_player4_connected():
-    global camra
-    camra = 4
-    update_camra()
-controller.player4.on_event(ControllerEvent.CONNECTED, on_player4_connected)
-
-def change_camra():
-    global camra
-    if camra == 1:
-        camra = 2
-    elif camra == 2:
-        camra = 3
-    elif camra == 3:
-        camra = 4
-    elif camra == 4:
-        camra = 1
-
-def on_player1_button_a_pressed():
-    player_1.say_text(player_1.x)
-    player_1.say_text(player_1.y, 1)
-controller.player1.on_button_event(ControllerButton.A,
-    ControllerButtonEvent.PRESSED,
-    on_player1_button_a_pressed)
-
-def on_player1_button_b_pressed():
-    change_camra()
-controller.player1.on_button_event(ControllerButton.B,
-    ControllerButtonEvent.PRESSED,
-    on_player1_button_b_pressed)
-
+camra = 0
+enemy_sprite: Sprite = None
+player_1: Sprite = None
+player_2: Sprite = None
+player_3: Sprite = None
+player_4: Sprite = None
+enemy_velocety = 0
+#player movement
 def move():
+    if controller.player1.is_pressed(ControllerButton.B):
+        change_camra()
     if controller.player1.is_pressed(ControllerButton.RIGHT):
         player_1.x += 2.5
     if controller.player1.is_pressed(ControllerButton.LEFT):
@@ -70,6 +49,87 @@ def move():
         player_3.y += 2.5
     if controller.player3.is_pressed(ControllerButton.UP):
         player_3.y += -2.5
+
+def on_player4_connected():
+    global camra
+    camra = 4
+    update_camra()
+    enemy_sprite.set_velocity(-30, 0)
+    enemy_sprite.set_position(300, 120)
+controller.player4.on_event(ControllerEvent.CONNECTED, on_player4_connected)
+
+
+def change_camra():
+    global camra
+    if camra == 1:
+        camra = 2
+    elif camra == 2:
+        camra = 3
+    elif camra == 3:
+        camra = 4
+    elif camra == 4:
+        camra = 1
+
+def on_player1_button_a_pressed():
+    player_1.say_text(player_1.x)
+    
+    def on_after():
+        player_1.say_text(player_1.y)
+    timer.after(1000, on_after)
+    
+controller.player1.on_button_event(ControllerButton.A,
+    ControllerButtonEvent.PRESSED,
+    on_player1_button_a_pressed)
+
+def start():
+    global camra, player_1, player_2, player_3, player_4, enemy_sprite, enemy_velocety
+    camra = 1
+    scene.camera_follow_sprite(player_1)
+    tiles.set_current_tilemap(tilemap("""
+        level1
+    """))
+    player_1 = sprites.create(assets.image("""
+        myImage1
+    """), SpriteKind.player1)
+    player_2 = sprites.create(assets.image("""
+        myImage0
+    """), SpriteKind.player2)
+    player_3 = sprites.create(assets.image("""
+        myImage2
+    """), SpriteKind.player3)
+    player_4 = sprites.create(assets.image("""
+        myImage
+    """), SpriteKind.cam)
+    enemy_sprite = sprites.create(img("""
+            ........................
+                    ........................
+                    ........................
+                    ........................
+                    ..........ffff..........
+                    ........ff1111ff........
+                    .......fb111111bf.......
+                    .......f1111111df.......
+                    ......fd1111111ddf......
+                    ......fd111111dddf......
+                    ......fd111ddddddf......
+                    ......fd1dfbddddbf......
+                    ......fbddfcdbbbcf......
+                    .......f11111bbcf.......
+                    .......f1b1fffff........
+                    .......fbfc111bf........
+                    ........ff1b1bff........
+                    .........fbfbfff.f......
+                    ..........ffffffff......
+                    ............fffff.......
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+        """),
+        SpriteKind.player)
+    enemy_velocety = -30
+    player_4.set_bounce_on_wall(False)
+
 def update_camra():
     if camra == 1:
         scene.camera_follow_sprite(player_1)
@@ -79,31 +139,12 @@ def update_camra():
         scene.camera_follow_sprite(player_3)
     elif camra == 4:
         scene.camera_follow_sprite(player_4)
-player_4: Sprite = None
-player_3: Sprite = None
-player_2: Sprite = None
-player_1: Sprite = None
-camra = 0
-camra = 1
-scene.camera_follow_sprite(player_1)
-tiles.set_current_tilemap(tilemap("""
-    level1
-"""))
-player_1 = sprites.create(assets.image("""
-    myImage1
-"""), SpriteKind.player1)
-player_2 = sprites.create(assets.image("""
-    myImage0
-"""), SpriteKind.player2)
-player_3 = sprites.create(assets.image("""
-    myImage2
-"""), SpriteKind.player3)
-player_4 = sprites.create(assets.image("""
-    myImage
-"""), SpriteKind.cam)
-player_4.set_bounce_on_wall(False)
 
 def on_on_update():
+    game_update()
+game.on_update(on_on_update)
+
+# game update code
+def game_update():
     update_camra()
     move()
-game.on_update(on_on_update)
--- main.ts
+++ main.ts
@@ -4,11 +4,99 @@ namespace SpriteKind {
     export const cam = SpriteKind.create()
     export const player3 = SpriteKind.create()
 }
-controller.player4.onEvent(ControllerEvent.Connected, function () {
+
+let camra = 0
+let enemy_sprite : Sprite = null
+let player_1 : Sprite = null
+let player_2 : Sprite = null
+let player_3 : Sprite = null
+let player_4 : Sprite = null
+let enemy_velocety = 0
+// player movement
+function move() {
+    if (controller.player1.isPressed(ControllerButton.B)) {
+        change_camra()
+    }
+    
+    if (controller.player1.isPressed(ControllerButton.Right)) {
+        player_1.x += 2.5
+    }
+    
+    if (controller.player1.isPressed(ControllerButton.Left)) {
+        player_1.x += -2.5
+    }
+    
+    if (controller.player1.isPressed(ControllerButton.Down)) {
+        player_1.y += 2.5
+    }
+    
+    if (controller.player1.isPressed(ControllerButton.Up)) {
+        player_1.y += -2.5
+    }
+    
+    if (controller.player2.isPressed(ControllerButton.Right)) {
+        player_2.x += 2.5
+    }
+    
+    if (controller.player2.isPressed(ControllerButton.Left)) {
+        player_2.x += -2.5
+    }
+    
+    if (controller.player2.isPressed(ControllerButton.Down)) {
+        player_2.y += 2.5
+    }
+    
+    if (controller.player2.isPressed(ControllerButton.Up)) {
+        player_2.y += -2.5
+    }
+    
+    if (controller.player4.isPressed(ControllerButton.Right)) {
+        player_4.x += 2.5
+    }
+    
+    if (controller.player4.isPressed(ControllerButton.Left)) {
+        player_4.x += -2.5
+    }
+    
+    if (controller.player4.isPressed(ControllerButton.Down)) {
+        player_4.y += 2.5
+    }
+    
+    if (controller.player4.isPressed(ControllerButton.Up)) {
+        player_4.y += -2.5
+    }
+    
+    if (controller.player3.isPressed(ControllerButton.Up)) {
+        player_3.y += -2.5
+    }
+    
+    if (controller.player3.isPressed(ControllerButton.Right)) {
+        player_3.x += 2.5
+    }
+    
+    if (controller.player3.isPressed(ControllerButton.Left)) {
+        player_3.x += -2.5
+    }
+    
+    if (controller.player3.isPressed(ControllerButton.Down)) {
+        player_3.y += 2.5
+    }
+    
+    if (controller.player3.isPressed(ControllerButton.Up)) {
+        player_3.y += -2.5
+    }
+    
+}
+
+controller.player4.onEvent(ControllerEvent.Connected, function on_player4_connected() {
+    
     camra = 4
     update_camra()
+    enemy_sprite.setVelocity(-30, 0)
+    enemy_sprite.setPosition(300, 120)
 })
-function change_camra () {
+function change_camra() {
+    
     if (camra == 1) {
         camra = 2
     } else if (camra == 2) {
@@ -18,70 +106,65 @@ function change_camra () {
     } else if (camra == 4) {
         camra = 1
     }
+    
 }
-controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function on_player1_button_a_pressed() {
     player_1.sayText(player_1.x)
-    timer.after(1000, function () {
+    timer.after(1000, function on_after() {
         player_1.sayText(player_1.y)
     })
 })
-controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
-    change_camra()
-})
-function move () {
-    if (controller.player1.isPressed(ControllerButton.Right)) {
-        player_1.x += 2.5
-    }
-    if (controller.player1.isPressed(ControllerButton.Left)) {
-        player_1.x += -2.5
-    }
-    if (controller.player1.isPressed(ControllerButton.Down)) {
-        player_1.y += 2.5
-    }
-    if (controller.player1.isPressed(ControllerButton.Up)) {
-        player_1.y += -2.5
-    }
-    if (controller.player2.isPressed(ControllerButton.Right)) {
-        player_2.x += 2.5
-    }
-    if (controller.player2.isPressed(ControllerButton.Left)) {
-        player_2.x += -2.5
-    }
-    if (controller.player2.isPressed(ControllerButton.Down)) {
-        player_2.y += 2.5
-    }
-    if (controller.player2.isPressed(ControllerButton.Up)) {
-        player_2.y += -2.5
-    }
-    if (controller.player4.isPressed(ControllerButton.Right)) {
-        player_4.x += 2.5
-    }
-    if (controller.player4.isPressed(ControllerButton.Left)) {
-        player_4.x += -2.5
-    }
-    if (controller.player4.isPressed(ControllerButton.Down)) {
-        player_4.y += 2.5
-    }
-    if (controller.player4.isPressed(ControllerButton.Up)) {
-        player_4.y += -2.5
-    }
-    if (controller.player3.isPressed(ControllerButton.Up)) {
-        player_3.y += -2.5
-    }
-    if (controller.player3.isPressed(ControllerButton.Right)) {
-        player_3.x += 2.5
-    }
-    if (controller.player3.isPressed(ControllerButton.Left)) {
-        player_3.x += -2.5
-    }
-    if (controller.player3.isPressed(ControllerButton.Down)) {
-        player_3.y += 2.5
-    }
-    if (controller.player3.isPressed(ControllerButton.Up)) {
-        player_3.y += -2.5
-    }
+function start() {
+    
+    camra = 1
+    scene.cameraFollowSprite(player_1)
+    tiles.setCurrentTilemap(tilemap`
+        level1
+    `)
+    player_1 = sprites.create(assets.image`
+        myImage1
+    `, SpriteKind.player1)
+    player_2 = sprites.create(assets.image`
+        myImage0
+    `, SpriteKind.player2)
+    player_3 = sprites.create(assets.image`
+        myImage2
+    `, SpriteKind.player3)
+    player_4 = sprites.create(assets.image`
+        myImage
+    `, SpriteKind.cam)
+    enemy_sprite = sprites.create(img`
+            ........................
+                    ........................
+                    ........................
+                    ........................
+                    ..........ffff..........
+                    ........ff1111ff........
+                    .......fb111111bf.......
+                    .......f1111111df.......
+                    ......fd1111111ddf......
+                    ......fd111111dddf......
+                    ......fd111ddddddf......
+                    ......fd1dfbddddbf......
+                    ......fbddfcdbbbcf......
+                    .......f11111bbcf.......
+                    .......f1b1fffff........
+                    .......fbfc111bf........
+                    ........ff1b1bff........
+                    .........fbfbfff.f......
+                    ..........ffffffff......
+                    ............fffff.......
+                    ........................
+                    ........................
+                    ........................
+                    ........................
+        `, SpriteKind.Player)
+    enemy_velocety = -30
+    player_4.setBounceOnWall(false)
 }
-function update_camra () {
+
+function update_camra() {
     if (camra == 1) {
         scene.cameraFollowSprite(player_1)
     } else if (camra == 2) {
@@ -91,53 +174,15 @@ function update_camra () {
     } else if (camra == 4) {
         scene.cameraFollowSprite(player_4)
     }
+    
 }
-let player_4: Sprite = null
-let player_3: Sprite = null
-let player_2: Sprite = null
-let player_1: Sprite = null
-let camra = 0
-camra = 1
-scene.cameraFollowSprite(player_1)
-tiles.setCurrentTilemap(tilemap`level1`)
-player_1 = sprites.create(assets.image`myImage1`, SpriteKind.player1)
-player_2 = sprites.create(assets.image`myImage0`, SpriteKind.player2)
-player_3 = sprites.create(assets.image`myImage2`, SpriteKind.player3)
-player_4 = sprites.create(assets.image`myImage`, SpriteKind.cam)
-let enemy_sprite = sprites.create(img`
-    ........................
-    ........................
-    ........................
-    ........................
-    ..........ffff..........
-    ........ff1111ff........
-    .......fb111111bf.......
-    .......f1111111df.......
-    ......fd1111111ddf......
-    ......fd111111dddf......
-    ......fd111ddddddf......
-    ......fd1dfbddddbf......
-    ......fbddfcdbbbcf......
-    .......f11111bbcf.......
-    .......f1b1fffff........
-    .......fbfc111bf........
-    ........ff1b1bff........
-    .........fbfbfff.f......
-    ..........ffffffff......
-    ............fffff.......
-    ........................
-    ........................
-    ........................
-    ........................
-    `, SpriteKind.Player)
-let enemy_velocety = -30
-enemy_sprite.setVelocity(-30, 0)
-enemy_sprite.setPosition(300, 120)
-player_4.setBounceOnWall(false)
-game.onUpdate(function () {
+
+game.onUpdate(function on_on_update() {
+    game_update()
+})
+//  game update code
+function game_update() {
     update_camra()
     move()
-    if (true) {
-    	
-    }
-})
+}
+
